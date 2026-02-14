412f25d53be0b01197618c7185386eb4dfb5bceb
Chinthaka Dissanayake <143018090+chinthakadd7@users.noreply.github.com>
2026-02-14 14:09:08 +0530

    ch

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -23,7 +23,7 @@ function Register() {
 
   return (
     <div>
-      <h2>Chinthaka</h2>
+      <h2>Chinthaka Dissanayake</h2>
       <form onSubmit={handleSubmit}>
         <input
           type="email"
